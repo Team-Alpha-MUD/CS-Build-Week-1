1230edf5a27a8eaee820280f4aa1de84fb88c836
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-07 14:56:38 -0500

    Testing

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
 
+from rest_framework import routers #added
+from notes.api import PersonalNoteViewSet #added
+from rest_framework.authtoken import views #added
+
+router = routers.DefaultRouter() #added
+router.register('notes', PersonalNoteViewSet) #added
+
 urlpatterns = [
     path('', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
