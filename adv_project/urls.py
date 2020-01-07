198b8cd785e87543e6e22282032cf6bad6da0b8f
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-07 14:31:04 -0500

    Still trying to set homepage

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf.urls import include
 
 urlpatterns = [
+    path('/', include('rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
