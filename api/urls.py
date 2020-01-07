e8c45cdfee0de72c6559ec36a7f8b39167acbee2
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-07 14:17:42 -0500

    Trying to make homepage

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
+    path('/', include('rest_auth.registration.urls')),
     path('registration/', include('rest_auth.registration.urls')),
 ]
