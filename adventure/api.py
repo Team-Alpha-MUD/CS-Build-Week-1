8d89f289e79c4f04bb900c8223ea4bdab6b029a2
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-08 14:09:10 -0500

    Fixing rooms route

--- adventure/api.py
+++ adventure/api.py
@@ -66,8 +66,7 @@ def say(request):
     # IMPLEMENT
     return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
 
-from django.core import serializers
 @api_view(["GET"])
 def rooms(request):
-    rooms = serializers.serialize("json", Room.objects.all())
-    return JsonResponse(rooms, safe=False)
+    rooms = Room.objects.all()
+    return JsonResponse({'rooms': list(rooms)}, safe=True)
