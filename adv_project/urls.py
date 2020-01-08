54e87f8fb9f375f0a2fb707d2b1c063a4ad80f55
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2020-01-08 10:51:16 -0500

    Fixing api

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -2,17 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
 
-from rest_framework import routers
-from adventure.api import PlayerViewSet, RoomViewSet
-
-router = routers.DefaultRouter()
-router.register(r'players', PlayerViewSet)
-router.register(r'rooms', RoomViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
-    path('api/', include(router.urls)),
 ]
--- adventure/urls.py
+++ adventure/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
 from . import api
+from django.urls import path, include
+
+from rest_framework import routers
+from adventure.api import PlayerViewSet, RoomViewSet
+
+router = routers.DefaultRouter()
+router.register(r'players', PlayerViewSet)
+router.register(r'rooms', RoomViewSet)
 
 urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
+    path('', include(router.urls)),
 ]
