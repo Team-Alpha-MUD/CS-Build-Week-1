955dd0cbdbe775339f137a82602983139737566d
George Hou <George Hou>
2020-01-09 10:40:05 -0800

    update homepage

--- adventure/views.py
+++ adventure/views.py
@@ -4,8 +4,8 @@ from .models import Room, Player
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("This is created by <strong>Django</strong>!")
-    # return render(request=request, 
-    #               template_name='adventure/home.html',
-    #               context={'room': Room.objects.all,
-    #                        'player': Player.objects.all})
+    # return HttpResponse("This is created by <strong>Django</strong>!")
+    return render(request=request, 
+                  template_name='adventure/home.html',
+                  context={'room': Room.objects.all,
+                           'player': Player.objects.all})
