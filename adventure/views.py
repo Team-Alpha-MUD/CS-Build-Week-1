21101bff1004c600ec26d6794ec4a547d8eaa12b
George Hou <George Hou>
2020-01-09 11:03:57 -0800

    hp

--- adventure/views.py
+++ adventure/views.py
@@ -6,6 +6,6 @@ from .models import Room, Player
 def homepage(request):
     # return HttpResponse("This is created by <strong>Django</strong>!")
     return render(request=request, 
-                  template_name='adventure/home.html')
+                  template_name='adventure/home.html',
                   context={'room': Room.objects.all,
                            'player': Player.objects.all})
